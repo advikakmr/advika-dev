f1f749c7e96d280e12bfe243f089960e159bba60
advika <157998786+advikakmr@users.noreply.github.com>
2025-07-29 08:19:55 -0700

    fix tab name

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,17 @@ set -e
 # Prompt for a commit message
 read -p ">> Enter commit message: " COMMIT_MSG
 
-echo ">> Building Hugo site..."
+echo -e "\n>> Building Hugo site...\n"
 hugo
 
-echo ">> Committing & pushing files to main..."
+echo -e "\n>> Committing & pushing files to main...\n"
 git add .
-git commit -m "$COMMIT_MSG" || echo ">> Nothing to commit."
+git commit -m "$COMMIT_MSG" || echo "\n>> Nothing to commit.\n"
 git push origin main
 
-echo ">> Committing & pushing static site to build..."
+echo -e "\n>> Committing & pushing static site to build...\n"
 git subtree split --prefix=public -b temp-deploy
 git push -f origin temp-deploy:build
 git branch -D temp-deploy
 
-echo ">> Done. Site deployed to build branch."
+echo -e "\n>> Done. Site deployed to build branch.\n"
